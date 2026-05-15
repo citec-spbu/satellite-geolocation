066cdac09c122d87e9baeabc324b82e2d009d9f3
storublevceva <dariastorublevceva@gmail.com>
2026-05-15 13:43:38 +0300

    Update __init__.py

--- drone-localization/core/models/dual_branch_swin/__init__.py
+++ drone-localization/core/models/dual_branch_swin/__init__.py
@@ -1,3 +1,3 @@
-from .model import DualBranchSwin, Gem_heat
+from .dual_branch_swin import DualBranchSwin, Gem_heat
 from .loss import CircleLoss
 from .dataset import SUESDataset, get_transforms, UniversalDataset
