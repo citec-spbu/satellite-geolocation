c5bd62c992d37188e800944784f2598ccea4ed4e
ysnlle <fakievdanil76@gmail.com>
2026-05-18 02:15:40 +0300

    add schemas validation

--- drone_localization/api/schemas/localization.py
+++ drone_localization/api/schemas/localization.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, Field
 
 class Coordinates(BaseModel):
     """Координаты в формате WGS84."""
-    lat: float = Field(..., description="Широта (latitude) в градусах, диапазон [-90, 90]")
-    lon: float = Field(..., description="Долгота (longitude) в градусах, диапазон [-180, 180]")
+    lat: float = Field(..., description="Широта (latitude) в градусах, диапазон [-90, 90]", ge=-90.0, le=90.0)
+    lon: float = Field(..., description="Долгота (longitude) в градусах, диапазон [-180, 180]", ge=-180.0, le=180.0)
 
 class LocalizationRequest(BaseModel):
     drone_image: str
